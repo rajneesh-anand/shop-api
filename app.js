f21d74fd9a1e63cee302e907b1b6b519f0f241d8
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2021-07-22 15:29:10 +0530

    payment-edit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
 
-var allowedDomains = ["https://vic.vercel.app", "http://localhost:3000"];
+var allowedDomains = ["https://gulshan.vercel.app", "http://localhost:3000"];
 app.use(
   cors({
     origin: function (origin, callback) {
